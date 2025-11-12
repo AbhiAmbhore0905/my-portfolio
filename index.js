2f9d56b4766e2d6b10575e356e49c072fc6055d2
Your Name <you@example.com>
2025-11-12 20:31:37 +0530

    updated backend with frontend integration

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
-// const path = require("path")
+const path = require("path")
 require("dotenv").config()
 const app = express()
 
 
-// app.use(express.static("dist"))
+app.use(express.static("dist"))
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://my-portfolio-rj5s.onrender.com",
     credentials: true
 }))
 
@@ -17,8 +18,8 @@ app.use(cors({
 app.use("/api/public", require("./route/public.route"))
 
 app.use("*", (req, res) => {
-    // res.sendFile(path.join(__dirname, "dist", "index.html"))
-    res.status(404).json({ message: "resource not found" })
+    // res.status(404).json({ message: "resource not found" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
 
 app.use("*", (err, req, res, next) => {
